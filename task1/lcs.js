59588ad75c6279c57e3698b43c88f39f61b418bb
Domas <domas0319@gmail.com>
2025-09-25 01:23:34 +0300

    improved

--- task1/lcs.js
+++ task1/lcs.js
@@ -1,15 +1,9 @@
-let a = process.argv.slice(2);
-if (!a.length) {
-  console.log("");
-} else {
-  let s = a[0], r = "";
-  for (let i = 0; i < s.length; i++) {
-    for (let j = i + 1; j <= s.length; j++) {
-      let sub = s.slice(i, j);
-      if (sub.length > r.length && a.every(x => x.includes(sub))) {
-        r = sub;
-      }
-    }
+let a=process.argv.slice(2),r=""
+if(a.length){
+  let s=a[0]
+  for(let i=0;i<s.length;i++)for(let j=i+1;j<=s.length;j++){
+    let sub=s.slice(i,j)
+    if(sub.length>r.length&&a.every(x=>x.includes(sub)))r=sub
   }
-  console.log(r);
 }
+console.log(r)
