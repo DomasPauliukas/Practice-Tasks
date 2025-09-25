8e78fb254aab3fb5655f7864a43553739bff1c79
Domas <domas0319@gmail.com>
2025-09-25 17:27:18 +0300

    task3-gcd/lcm

---
+++ task3/task3.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const url = require('url')
+
+function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b)
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b)
+}
+
+const email = "domas0319_gmail_com"
+const server = http.createServer((req, res) => {
+    const q = url.parse(req.url, true)
+    if (q.pathname === '/' + email) {
+        const x = parseInt(q.query.x)
+        const y = parseInt(q.query.y)
+        if (x > 0 && y > 0) {
+            res.end(String(lcm(x, y)))
+        } else {
+            res.end('NaN')
+        }
+    } else {
+        res.end('Invalid endpoint')
+    }
+})
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000')
+})
